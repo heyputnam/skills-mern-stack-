f2972e22915c03df67dc94af29b9063b6f5d22b9
heyputnam <heyputnam@gmail.com>
2021-04-05 13:58:07 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// require modules
+
+const express = require('express');
+const morgan = require('morgan')
+
+
+// inital express setup
+
+// config setting with app.set()
+
+
+//mount middleware with app.use()
+
+
+// mount routes with app.use()
+
+
+//tell port listening on port 3001
